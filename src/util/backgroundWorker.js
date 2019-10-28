ad1a3459293effe1b3476ad19afb50a087416bd2
William Swanson <swansontec@gmail.com>
2019-10-28 16:33:32 -0700

    Refactor the background worker for clarity

--- src/util/backgroundWorker.js
+++ src/util/backgroundWorker.js
@@ -12,48 +12,66 @@ import s from '../locales/strings.js'
 
 export async function backgroundWorker () {
   console.log('appStateLog: running background task')
-  const lastNotif = await AsyncStorage.getItem(Constants.LOCAL_STORAGE_BACKGROUND_PUSH_KEY)
+
+  // Only run once per day:
+  const last24HourNotif = await AsyncStorage.getItem(Constants.LOCAL_STORAGE_BACKGROUND_PUSH_KEY)
   const now = new Date()
-  if (lastNotif) {
-    const lastNotifDate = new Date(lastNotif).getTime() / 1000
+  if (last24HourNotif) {
+    const lastNotifDate = new Date(last24HourNotif).getTime() / 1000
     const delta = now.getTime() / 1000 - lastNotifDate
     if (delta < Constants.PUSH_DELAY_SECONDS) {
       BackgroundFetch.finish()
       return
     }
   }
-  try {
-    const result = await fetchLoginMessages(ENV.AIRBITZ_API_KEY)
-    const date = new Date(Date.now() + 1000)
-    // for each key
-    for (const key in result) {
-      // skip loop if the property is from prototype
-      if (!result.hasOwnProperty(key)) continue
-      const obj = result[key]
-      if (obj.otpResetPending) {
-        if (Platform.OS === Constants.IOS) {
-          PushNotification.localNotificationSchedule({
-            title: s.strings.otp_notif_title,
-            message: sprintf(s.strings.otp_notif_body, key),
-            date
-          })
-        } else {
-          PushNotification.localNotificationSchedule({
-            message: s.strings.otp_notif_title,
-            subText: sprintf(s.strings.otp_notif_body, key),
-            date
-          })
-        }
-      }
-    }
-  } catch (error) {
-    global.bugsnag.notify(error)
-    console.error(error)
-  }
   await AsyncStorage.setItem(Constants.LOCAL_STORAGE_BACKGROUND_PUSH_KEY, now.toString())
 
-  // Required: Signal completion of your task to native code
-  // If you fail to do this, the OS can terminate your app
-  // or assign battery-blame for consuming too much background-time
-  BackgroundFetch.finish(BackgroundFetch.FETCH_RESULT_NEW_DATA)
+  // Perform tasks:
+  return showLoginMessages().then(
+    () => {
+      // Required: Signal completion of your task to native code
+      // If you fail to do this, the OS can terminate your app
+      // or assign battery-blame for consuming too much background-time
+      BackgroundFetch.finish(BackgroundFetch.FETCH_RESULT_NEW_DATA)
+    },
+    error => {
+      console.error(error)
+      global.bugsnag.notify(error)
+      BackgroundFetch.finish(BackgroundFetch.FETCH_RESULT_FAILED)
+    }
+  )
+}
+
+/**
+ * Shows OTP and other messages for users on this device.
+ */
+async function showLoginMessages () {
+  const loginMessages = await fetchLoginMessages(ENV.AIRBITZ_API_KEY)
+
+  for (const username of Object.keys(loginMessages)) {
+    const messages = loginMessages[username]
+    if (messages.otpResetPending) {
+      showSystemNotification(s.strings.otp_notif_title, sprintf(s.strings.otp_notif_body, username))
+    }
+  }
+}
+
+/**
+ * Helper function to a show system-level notifications.
+ */
+function showSystemNotification (title: string, message: string) {
+  const date = new Date(Date.now() + 1000)
+  if (Platform.OS === 'android') {
+    PushNotification.localNotificationSchedule({
+      message: title,
+      subText: message,
+      date
+    })
+  } else {
+    PushNotification.localNotificationSchedule({
+      title,
+      message,
+      date
+    })
+  }
 }
