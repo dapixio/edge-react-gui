23a6afe276764f54cbfb74620f7a238942e24765
andreyvEze <andreyv@eze.tech>
2020-02-26 17:05:36 +0200

    Intl fix. 1.0.1 android, 1.0.0 5 ios.

--- android/app/src/main/java/co/edgesecure/app/MainApplication.java
+++ android/app/src/main/java/co/edgesecure/app/MainApplication.java
@@ -56,7 +56,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
-            new NetInfoPackage(),
               new NetInfoPackage(),
               new ARTPackage(),
               new AsyncStoragePackage(),
